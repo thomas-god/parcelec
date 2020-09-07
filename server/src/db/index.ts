89cc71ded3b2e3c2b482998f197695fe92c771ad
thomas-god <thomas.godlewski@icloud.com>
2020-09-07 15:13:17 +0200

    Add basic error callback to pg pool errors

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -5,6 +5,13 @@ const databaseConfig = {
 };
 const pool = new Pool(databaseConfig);
 
+pool.on("error", (err, client) => {
+  console.log("#########################################");
+  console.log("######## Error with the database ########");
+  console.log(client);
+  console.log(err);
+});
+
 export default {
   query: (text: string, params: any): Promise<QueryResult> =>
     pool.query(text, params),
