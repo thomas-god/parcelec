a5f57608005aff7530218af5ea893caf8a713437
thomas-god <thomas.godlewski@icloud.com>
2020-09-13 16:42:45 +0200

    Add the users list to the auction store module

--- client/src/store/auction.ts
+++ client/src/store/auction.ts
@@ -5,6 +5,7 @@ export interface Auction {
   name: string;
   id: string;
   status: "Open" | "Running" | "Close";
+  users: string[];
 }
 
 export interface AuctionState extends Auction {}
@@ -14,21 +15,34 @@ export const state: AuctionState = {
   name: "",
   id: "",
   status: "Close",
+  users: [],
 };
 
 // ------------------------ ACTIONS -------------------------
 export const actions: ActionTree<AuctionState, RootState> = {
-  setAuction({ commit }, payload: Auction): void {
-    commit("SET_AUCTION", payload);
+  async setAuction({ commit }, payload: Auction): Promise<void> {
+    const users = await getUsersList(payload.id);
+    commit("SET_AUCTION", { ...payload, users });
+  },
+  async updateUsersList({ state, commit }): Promise<void> {
+    const users = await getUsersList(state.id);
+    commit("SET_USERS", users);
   },
 };
 
 // ------------------------ MUTATIONS -------------------------
 export const mutations: MutationTree<AuctionState> = {
-  SET_AUCTION(state, payload: Auction): void {
-    state.id = payload.id;
-    state.name = payload.name;
-    state.status = payload.status;
+  SET_AUCTION(state, auction: Auction): void {
+    state.id = auction.id;
+    state.name = auction.name;
+    state.status = auction.status;
+    state.users = auction.users;
+  },
+  SET_USERS(state, users: string[]): void {
+    state.users = users;
+  },
+  PUSH_NEW_USER(state, new_user: string): void {
+    state.users.push(new_user);
   },
 };
 
@@ -53,3 +67,16 @@ export const auction: Module<AuctionState, RootState> = {
   actions,
   mutations,
 };
+
+// ------------------------ Helper functions ---------------
+async function getUsersList(auction_id: string): Promise<string[]> {
+  const res = await fetch(`http://localhost:3000/auction/${auction_id}`, {
+    method: "GET",
+  });
+  if (res.status === 200) {
+    const body = await res.json();
+    return body.users as string[];
+  } else {
+    return [];
+  }
+}
