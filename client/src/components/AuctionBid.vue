781cc92d403bdf7cd35a6c16de8e9657e8bb6f1c
thomas-god <thomas.godlewski@icloud.com>
2020-09-14 17:25:45 +0200

    Add a submit bid component

---
+++ client/src/components/AuctionBid.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="bid__box">
+    <h2>Votre enchère</h2>
+    <input type="number" :disabled="!can_bid" v-model="bid_value" />
+    <button @click="submitBid" :disabled="!can_bid">Soumettre</button>
+    <span v-if="bid_value_err" style="color: red">{{ bid_value_err_msg }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { State, Action, Getter, namespace } from "vuex-class";
+import { Auction } from "../store/auction";
+
+const auctionModule = namespace("auction");
+const userModule = namespace("user");
+
+@Component
+export default class Bid extends Vue {
+  @auctionModule.Getter auction!: Auction;
+  @auctionModule.Getter can_bid!: boolean;
+  @auctionModule.Action updateBidAbility!: (bid_ability: boolean) => void;
+  @userModule.Getter user_id!: string;
+
+  private bid_value = 0;
+  private bid_value_err = false;
+  private bid_value_err_msg = "";
+
+  async submitBid(): Promise<void> {
+    console.log(this.bid_value);
+    const res = await fetch(
+      `http://localhost:3000/auction/${this.auction.id}/bid`,
+      {
+        method: "PUT",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({ user_id: this.user_id, bid: this.bid_value }),
+      }
+    );
+    if (res.status === 201) {
+      this.bid_value_err = false;
+      this.bid_value_err_msg = "";
+      this.updateBidAbility(false);
+    } else {
+      this.bid_value_err = true;
+      this.bid_value_err_msg = await res.text();
+    }
+  }
+}
+</script>
+
+<style scoped>
+.bid__box {
+  margin: 10px auto;
+  padding: 2rem;
+  max-width: 350px;
+
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+}
+
+.bid__box input {
+  font-size: 2rem;
+  text-align: center;
+}
+
+.bid__box button {
+  margin: 10px auto;
+  flex-grow: 0;
+  max-width: 200px;
+  font-size: 1.3rem;
+}
+</style>
