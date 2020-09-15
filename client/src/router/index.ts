8efef7d1b52797d64fb6fc7886fae801b0f413b1
thomas-god <thomas.godlewski@icloud.com>
2020-09-15 07:58:12 +0200

    Add a route hanlder to restore a user session

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "../views/Home.vue";
+import store from "../store/";
 
 Vue.use(VueRouter);
 
@@ -8,23 +9,49 @@ const routes: Array<RouteConfig> = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    path: "/auction/:auction_id/user/:user_id",
+    beforeEnter: async (to, from, next) => {
+      const auction_id: string = to.params.auction_id;
+      const user_id: string = to.params.user_id;
+
+      const res_user = await fetch(
+        `${process.env.VUE_APP_API_URL}/auction/${auction_id}/user/${user_id}`,
+        {
+          method: "GET",
+        }
+      );
+      const res_auction = await fetch(
+        `${process.env.VUE_APP_API_URL}/auction/${auction_id}`,
+        {
+          method: "GET",
+        }
+      );
+      if (res_user.status === 200 && res_auction.status === 200) {
+        const user = await res_user.json();
+        const auction = await res_auction.json();
+        await store.dispatch("user/setUsername", user.name, { root: true });
+        await store.dispatch(
+          "auction/setAuction",
+          { id: auction_id, name: auction.name, status: auction.status },
+          { root: true }
+        );
+        await store.dispatch("user/setUserID", String(user_id), { root: true });
+        await store.dispatch("auction/updateBidAbility", user.can_bid, {
+          root: true,
+        });
+      }
+      next("/");
+    },
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
