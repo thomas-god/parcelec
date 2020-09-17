d707c74c3f1100b520ca4dba71f012cc2ac3d19a
thomas-god <thomas.godlewski@icloud.com>
2020-09-17 10:25:53 +0200

    Add styling for power plants list

--- client/src/components/PowerPlantItem.vue
+++ client/src/components/PowerPlantItem.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="pp__grid">
-    <div class="pp__logo">{{ power_plant.type }}</div>
-    <div class="pp__barre">
-      {{ power_plant.p_min_mw }}, {{ power_plant.p_max_mw }}
+    <div class="pp__logo">{{ logo }}</div>
+    <div :style="pp__bare_style">
+      <div :style="pp__bare__p_min" class="pp__bare__p_min">
+        <p>{{ power_plant.p_min_mw }}</p>
+      </div>
+      <div class="pp__bare__p_max">
+        <p>{{ power_plant.p_max_mw }}</p>
+      </div>
     </div>
     <div class="pp__infos">
-      {{ power_plant.price_eur_per_mwh }}, {{ power_plant.stock_max_mwh }}
+      Coût : <strong>{{ power_plant.price_eur_per_mwh }}</strong> €/MWh, Stock :
+      <strong>{{ stock }}</strong> MWh
     </div>
   </div>
 </template>
@@ -18,6 +24,39 @@ import { PowerPlant } from "../store/portfolio";
 @Component
 export default class PowerPlantItem extends Vue {
   @Prop() power_plant!: PowerPlant;
+  @Prop() power_max_mw!: number;
+
+  get stock(): string {
+    return this.power_plant.stock_max_mwh === -1
+      ? "∞"
+      : String(this.power_plant.stock_max_mwh);
+  }
+
+  get logo(): string {
+    let logo = "";
+    if (this.power_plant.type === "nuc") logo = "☢️";
+    if (this.power_plant.type === "therm") logo = "🔥";
+    if (this.power_plant.type === "hydro") logo = "💧";
+    if (this.power_plant.type === "ren") logo = "☀️";
+    if (this.power_plant.type === "storage") logo = "🔋";
+    return logo;
+  }
+
+  get pp__bare_style(): string {
+    return `
+      position: relative;
+      grid-area: barre;
+      border: 2px solid rgb(0, 195, 255);
+      border-radius: 2px;
+      width: ${(this.power_plant.p_max_mw / this.power_max_mw) * 100}%
+    `;
+  }
+
+  get pp__bare__p_min(): string {
+    return `
+      width: ${(this.power_plant.p_min_mw / this.power_plant.p_max_mw) * 100}%;
+    `;
+  }
 }
 </script>
 
@@ -33,14 +72,51 @@ export default class PowerPlantItem extends Vue {
 
 .pp__logo {
   grid-area: logo;
+  align-self: center;
+  text-align: center;
+  font-size: 2rem;
 }
 
 .pp__barre {
   grid-area: barre;
-  border: 1px solid red;
+  border: 1px solid rgb(136, 184, 199);
+  border-radius: 2px;
+}
+
+.pp__bare__p_max {
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.pp__bare__p_min {
+  position: relative;
+  height: 100%;
+  text-align: end;
+  border-right: 2px dashed black;
+  background: repeating-linear-gradient(
+    -45deg,
+    #c8cad4,
+    #c8cad4 10px,
+    #a2a4aa 10px,
+    #a2a4aa 20px
+  );
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.pp__bare__p_min p,
+.pp__bare__p_max p {
+  margin: 0;
+  padding-right: 5px;
+  font-weight: bold;
 }
 
 .pp__infos {
   grid-area: infos;
+  align-self: center;
 }
 </style>
--- client/src/components/PowerPlantsList.vue
+++ client/src/components/PowerPlantsList.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="pp__list">
-    <PowerPlantItem v-for="pp in power_plants" :power_plant="pp" :key="pp.id" />
+    <h2>Votre parc de production</h2>
+    <PowerPlantItem
+      class="pp__list_item"
+      v-for="pp in power_plants"
+      :key="pp.id"
+      :power_plant="pp"
+      :power_max_mw="power_plants_max_power_mw"
+    />
   </div>
 </template>
 
@@ -17,11 +24,22 @@ const portfolioModule = namespace("portfolio");
 @Component({ components: { PowerPlantItem } })
 export default class Main extends Vue {
   @portfolioModule.Getter power_plants!: PowerPlant[];
+
+  get power_plants_max_power_mw(): number {
+    return this.power_plants.sort((a, b) => b.p_max_mw - a.p_max_mw)[0]
+      .p_max_mw;
+  }
 }
 </script>
 
 <style scoped>
 .pp__list {
   max-width: 400px;
+  border: 2px solid gray;
+  border-radius: 2px;
+}
+
+.pp__list_item {
+  margin: 1rem;
 }
 </style>
