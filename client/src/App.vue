d8cb2d3f896b2443f51591cb688878a2177724b4
thomas-god <thomas.godlewski@icloud.com>
2020-09-22 13:55:26 +0200

    Add app navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,26 @@
 <template>
   <div id="app">
+    <navbar />
     <router-view />
   </div>
 </template>
 
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import Navbar from "./components/Navbar.vue";
+
+@Component({
+  components: {
+    Navbar
+  }
+})
+export default class App extends Vue {}
+</script>
+
 <style>
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Accueil ⚡️</router-link>
+    |
+    <router-link to="/join">Rejoindre une partie</router-link>
+    |
+    <router-link to="/create">Créer une partie</router-link>
+  </div>
+</template>
+
+<style scoped>
+#nav {
+  width: 100%;
+  height: 34px;
+  background-color: rgb(204, 218, 250);
+  border-bottom: 1px solid black;
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  display: flex;
+  align-items: center;
+  z-index: 10;
+}
+a {
+  margin: auto 1rem;
+  align-self: center;
+  text-decoration: none;
+  color: black;
+  font-size: 1.2rem;
+}
+</style>
