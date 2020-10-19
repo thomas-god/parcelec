ac8c8f86a62f6ee094ec8347c0b4261104551486
thomas-god <thomas.godlewski@icloud.com>
2020-10-19 11:26:31 +0200

    Add API tests for reject/accepte OTC routes

--- server/tests/integration/routes_otc.spec.ts
+++ server/tests/integration/routes_otc.spec.ts
@@ -6,7 +6,6 @@
  *  POST /session/:session_id/user/:user_id/otc/new
  *  PUT /session/:session_id/user/:user_id/otc/:otc_id/accept
  *  PUT /session/:session_id/user/:user_id/otc/:otc_id/reject
- *  PUT /session/:session_id/user/:user_id/otc/:otc_id/counter_offer
  *
  */
 import { v4 as uuid, validate as uuidValidate } from "uuid";
@@ -59,7 +58,7 @@ describe("Posting an OTC offer to another user", () => {
 /**
  * GET /session/:session_id/user/:user_id/otc
  */
-describe("Posting an OTC offer to another user", () => {
+describe("Getting a user's OTC list", () => {
   let session_id: string;
   let user_id: string;
   beforeEach(async () => {
@@ -69,7 +68,7 @@ describe("Posting an OTC offer to another user", () => {
     user_id = await insertNewUser(session_id, "User");
   });
 
-  test("Should post a simple OTC offer", async () => {
+  test("Should get a single OTC item", async () => {
     try {
       const { session_id, user_id_1 } = await insertRunningSession(
         "Running Session"
@@ -95,6 +94,168 @@ describe("Posting an OTC offer to another user", () => {
       expect(uuidValidate(res.body[0].user_to)).toEqual(false);
     } catch (error) {
       fail(error);
+    }
+  });
+});
+
+/**
+ * PUT /session/:session_id/user/:user_id/otc/:otc_id/accept
+ */
+describe("Accepting an OTC offer", () => {
+  let session_id: string;
+  let user_id: string;
+  beforeEach(async () => {
+    await clearDB();
+    await getDefaultScenarioID();
+    session_id = await insertNewSession("Session");
+    user_id = await insertNewUser(session_id, "User");
+  });
+
+  test("Should accept the offer as the to user", async () => {
+    try {
+      const { session_id, user_id_1, user_id_2 } = await insertRunningSession(
+        "Running Session"
+      );
+      const otc_id = (
+        await superagent
+          .post(`${url}/session/${session_id}/user/${user_id_1}/otc`)
+          .send({
+            type: "sell",
+            user_to: "User 2",
+            volume_mwh: 100,
+            price_eur_per_mwh: 50,
+          })
+      ).body.otc_id;
+      const res = await superagent.put(
+        `${url}/session/${session_id}/user/${user_id_2}/otc/${otc_id}/accept`
+      );
+
+      expect(res.status).toEqual(200);
+    } catch (error) {
+      fail(error);
+    }
+  });
+
+  test("Should not be able to accept the offer as the from user", async () => {
+    try {
+      const { session_id, user_id_1, user_id_2 } = await insertRunningSession(
+        "Running Session"
+      );
+      const otc_id = (
+        await superagent
+          .post(`${url}/session/${session_id}/user/${user_id_1}/otc`)
+          .send({
+            type: "sell",
+            user_to: "User 2",
+            volume_mwh: 100,
+            price_eur_per_mwh: 50,
+          })
+      ).body.otc_id;
+      await superagent.put(
+        `${url}/session/${session_id}/user/${user_id_1}/otc/${otc_id}/accept`
+      );
+    } catch (error) {
+      expect(error.response.text).toEqual(
+        "Error, not allowed to modify this OTC"
+      );
+      expect(error.status).toEqual(403);
+    }
+  });
+
+  test("Should not be able to accept an already accepted offer", async () => {
+    try {
+      const { session_id, user_id_1, user_id_2 } = await insertRunningSession(
+        "Running Session"
+      );
+      const otc_id = (
+        await superagent
+          .post(`${url}/session/${session_id}/user/${user_id_1}/otc`)
+          .send({
+            type: "sell",
+            user_to: "User 2",
+            volume_mwh: 100,
+            price_eur_per_mwh: 50,
+          })
+      ).body.otc_id;
+      await superagent.put(
+        `${url}/session/${session_id}/user/${user_id_2}/otc/${otc_id}/accept`
+      );
+      await superagent.put(
+        `${url}/session/${session_id}/user/${user_id_2}/otc/${otc_id}/accept`
+      );
+    } catch (error) {
+      expect(error.response.text).toEqual(
+        "Error, OTC has already been accepted/rejected"
+      );
+      expect(error.status).toEqual(400);
+    }
+  });
+});
+
+/**
+ * PUT /session/:session_id/user/:user_id/otc/:otc_id/reject
+ */
+describe("Rejecting an OTC offer", () => {
+  let session_id: string;
+  let user_id: string;
+  beforeEach(async () => {
+    await clearDB();
+    await getDefaultScenarioID();
+    session_id = await insertNewSession("Session");
+    user_id = await insertNewUser(session_id, "User");
+  });
+
+  test("Should reject the offer as the to user", async () => {
+    try {
+      const { session_id, user_id_1, user_id_2 } = await insertRunningSession(
+        "Running Session"
+      );
+      const otc_id = (
+        await superagent
+          .post(`${url}/session/${session_id}/user/${user_id_1}/otc`)
+          .send({
+            type: "sell",
+            user_to: "User 2",
+            volume_mwh: 100,
+            price_eur_per_mwh: 50,
+          })
+      ).body.otc_id;
+      const res = await superagent.put(
+        `${url}/session/${session_id}/user/${user_id_2}/otc/${otc_id}/reject`
+      );
+
+      expect(res.status).toEqual(200);
+    } catch (error) {
+      fail(error);
+    }
+  });
+
+  test("Should not be able to reject an already rejected offer", async () => {
+    try {
+      const { session_id, user_id_1, user_id_2 } = await insertRunningSession(
+        "Running Session"
+      );
+      const otc_id = (
+        await superagent
+          .post(`${url}/session/${session_id}/user/${user_id_1}/otc`)
+          .send({
+            type: "sell",
+            user_to: "User 2",
+            volume_mwh: 100,
+            price_eur_per_mwh: 50,
+          })
+      ).body.otc_id;
+      await superagent.put(
+        `${url}/session/${session_id}/user/${user_id_2}/otc/${otc_id}/reject`
+      );
+      await superagent.put(
+        `${url}/session/${session_id}/user/${user_id_2}/otc/${otc_id}/reject`
+      );
+    } catch (error) {
+      expect(error.response.text).toEqual(
+        "Error, OTC has already been accepted/rejected"
+      );
+      expect(error.status).toEqual(400);
     }
   });
 });
