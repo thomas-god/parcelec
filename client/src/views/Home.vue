5ba3ee4500e1a1a959a21024a8d14d0f4616004d
thomas-god <thomas.godlewski@icloud.com>
2020-10-20 14:27:50 +0200

    Update Home page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -7,22 +7,6 @@
       vos clients en utilisant le plus efficacement possible les centrales en
       votre possession et en ayant recours si besoin au marché de l'électricité.
     </p>
-    <h2>Déroulé d'une partie</h2>
-    <p>
-      Une partie se compose d'une succession de phases dans lesquelles vous
-      serez confronté·e à différents niveaux de consommation.
-    </p>
-    <p>
-      Au début de chaque phase vos équipes commerciales vous indiqueront quel
-      niveau de consommation vous devrez couvrir (attention tout MWh non couvert
-      vous expose à une pénalité financière !).
-    </p>
-    <p>
-      Il vous faudra alors régler les points de consigne de vos centrales et
-      acheter ou vendre de l'énergie à d'autres producteurs pour répondre au
-      mieux à ce besoin. À la fin d'une phase vous recevrez un bilan de
-      performances énergétiques et financières.
-    </p>
 
     <div class="home__actions">
       <Btn font_size="1.2rem" @click="$router.push('/tutorial')">
@@ -76,6 +60,6 @@ export default class Home extends Vue {}
 
 .home__actions button {
   width: 140px;
-  margin: 0 1rem;
+  margin: 0.5rem 1rem;
 }
 </style>
