f1431f2c9067827706d8d1e85b089441b4410980
thomas-god <thomas.godlewski@icloud.com>
2020-10-31 10:24:42 +0100

    Add a forecast graph component for conso

---
+++ client/src/components/Forecast.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h2>Prévison de consommation</h2>
+    <ForecastGraph
+      class="chart"
+      :data="conso_forecast"
+      :line_title="'Consommation'"
+      :y_title="'Consommation (MWh)'"
+    />
+  </div>
+</template>
+
+<script lang='ts'>
+import { Component, Vue } from "vue-property-decorator";
+import { namespace } from "vuex-class";
+import ForecastGraph from "./ForecastGraph.vue";
+
+const portfolio_module = namespace("portfolio");
+
+@Component({ components: { ForecastGraph } })
+export default class Forecast extends Vue {
+  @portfolio_module.Getter conso_forecast!: number[];
+}
+</script>
+
+<style scoped>
+h2 {
+  margin-top: 0;
+  margin-bottom: 10px;
+}
+.chart {
+  position: relative;
+  max-width: 100%;
+  height: 300px;
+  margin-left: 0;
+  padding: 5px 10px 5px 0;
+}
+@media screen and (min-width: 400px) {
+  .chart {
+    width: 80vw;
+  }
+}
+@media screen and (max-width: 400px) {
+  .chart {
+    width: 95vw;
+  }
+}
+</style>
