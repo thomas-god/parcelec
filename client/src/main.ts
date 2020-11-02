d725c6daf34969d7f86490e479c496a58fed821c
thomas-god <thomas.godlewski@icloud.com>
2020-11-02 18:33:21 +0100

    Fix chartjs global font

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,6 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import { Chart } from "chart.js";
+
+Chart.defaults.global.defaultFontFamily = '"Montserrat"';
+Chart.defaults.global.defaultFontColor = "black";
+Chart.defaults.global.defaultFontSize = 15;
+Chart.defaults.global.defaultFontStyle = "normal";
+
 Vue.config.productionTip = false;
 
 new Vue({
