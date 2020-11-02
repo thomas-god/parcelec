dd0f9c709aa985eadb64249001e226156a5dd879
thomas-god <thomas.godlewski@icloud.com>
2020-11-02 16:18:21 +0100

    Add stocks graph on data tab

--- client/src/components/MainData.vue
+++ client/src/components/MainData.vue
@@ -1,16 +1,20 @@
-<template> 
-<div>
-    <MainDataForecast/>
-    <MainDataPlannings/>
-</div>
+<template>
+  <div>
+    <MainDataForecast />
+    <MainDataPlannings />
+    <MainDataStocks />
+  </div>
 </template>
 
 <script lang='ts'>
 import { Component, Vue } from "vue-property-decorator";
 import MainDataForecast from "./MainDataForecast.vue";
 import MainDataPlannings from "./MainDataPlannings.vue";
+import MainDataStocks from "./MainDataStocks.vue";
 
-@Component({ components: { MainDataForecast, MainDataPlannings } })
+@Component({
+  components: { MainDataForecast, MainDataPlannings, MainDataStocks }
+})
 export default class MainData extends Vue {}
 </script>
 
