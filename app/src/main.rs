e8c1fa2ac271a224ee385cc18727f35549d67c8f
thomas-god <thomas.godlewski@icloud.com>
2025-02-03 20:36:34 +0100

    feat: basic implementation of a ws server

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,6 +1,47 @@
+use futures_util::{SinkExt, StreamExt};
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::{
+    accept_async,
+    tungstenite::{Error, Result},
+};
+
 pub mod market;
 pub mod order_book;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let addr = "127.0.0.1:9002";
+    let listener = TcpListener::bind(&addr).await.expect("Unable to listen");
+    println!("Listenning on {addr}");
+
+    while let Ok((stream, _)) = listener.accept().await {
+        let peer = stream.peer_addr().expect("No peer address");
+        println!("Connection from {peer}");
+
+        tokio::spawn(accept_connection(stream));
+    }
+}
+
+async fn accept_connection(stream: TcpStream) {
+    let peer = stream.peer_addr().unwrap();
+    if let Err(e) = handle_connection(stream).await {
+        match e {
+            Error::ConnectionClosed | Error::Protocol(_) | Error::Utf8 => (),
+            err => println!("Error processing connection: {}", err),
+        }
+    }
+    println!("Connection with {peer} closed");
+}
+
+async fn handle_connection(stream: TcpStream) -> Result<()> {
+    let mut ws_stream = accept_async(stream).await.expect("Failed to accept");
+
+    while let Some(msg) = ws_stream.next().await {
+        let msg = msg?;
+        if msg.is_text() || msg.is_binary() {
+            ws_stream.send(msg).await?;
+        }
+    }
+
+    Ok(())
 }
