21db152ba181fdaa1b843b2baf36561cefb15899
thomas-god <thomas.godlewski@icloud.com>
2025-02-04 14:08:14 +0100

    feat: ugly implementation of an UI for the order book

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,83 +1,115 @@
 <script lang="ts">
-  import svelteLogo from "./assets/svelte.svg";
-  import viteLogo from "/vite.svg";
-  import Counter from "./lib/Counter.svelte";
-
+  import OrderBookEntry from "./lib/orderBookEntry.svelte";
   const socket = new WebSocket("ws://127.0.0.1:9002");
-  let snapshot = $state({
+  let price: number = $state(50);
+  let volume: number = $state(100);
+  interface OrderEntry {
+    direction: "Buy" | "Sell";
+    volume: number;
+    price: number;
+    created_at: Date;
+  }
+  interface OrderBook {
+    bids: OrderEntry[];
+    offers: OrderEntry[];
+  }
+  let snapshot: OrderBook = $state({
     bids: [],
     offers: [],
+  });
+  const spread = $derived.by(() => {
+    if (snapshot.bids.length === 0 || snapshot.offers.length === 0) {
+      return Number.NaN;
+    }
+    return (snapshot.offers[0].price - snapshot.bids[0].price) / 100;
   });
   socket.addEventListener("message", (msg) => {
     let data = JSON.parse(msg.data)["OrderBookSnapshot"];
     console.log(data);
     snapshot = data;
+    snapshot.bids.sort((a, b) => b.price - a.price);
+    snapshot.offers.sort((a, b) => a.price - b.price);
   });
-  let orderRequest = {
-    price: 100,
-    volume: 10,
-    direction: "Buy",
-    owner: "toto",
-  };
 
-  let orderRequestPayload = $derived(
-    JSON.stringify({ OrderRequest: orderRequest })
-  );
-  const sendRequest = () => {
-    console.log(`sending order request: ${orderRequestPayload}`);
-    socket.send(orderRequestPayload);
+  const sendBuyRequest = () => {
+    const orderRequest = {
+      price: price * 100,
+      volume,
+      direction: "Buy",
+      owner: "toto",
+    };
+    const payload = JSON.stringify({ OrderRequest: orderRequest });
+    console.log(`sending order request: ${payload}`);
+    socket.send(payload);
+  };
+  const sendSellRequest = () => {
+    const orderRequest = {
+      price: price * 100,
+      volume,
+      direction: "Sell",
+      owner: "toto",
+    };
+    const payload = JSON.stringify({ OrderRequest: orderRequest });
+    console.log(`sending order request: ${payload}`);
+    socket.send(payload);
   };
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <h2>Order book</h2>
+  {#if !Number.isNaN(spread)}
+    Spread: {spread} €
+  {/if}
+  <div class="OrderBook">
+    <div class="OrderBookColumn">
+      <h3>Achats</h3>
+      <ul class="OrderBook">
+        {#each snapshot.bids as bid}
+          <li>
+            <OrderBookEntry price={bid.price} volume={bid.volume} />
+          </li>
+        {/each}
+      </ul>
+    </div>
+
+    <div class="OrderBookColumn">
+      <h3>Ventes</h3>
+      <ul class="OrderBook">
+        {#each snapshot.offers as offer}
+          <li>
+            <OrderBookEntry price={offer.price} volume={offer.volume} />
+          </li>
+        {/each}
+      </ul>
+    </div>
   </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
+  <div class="SendOrder">
+    <div>
+      <label>
+        Price
+        <input type="number" bind:value={price} />
+      </label>
+
+      <label>
+        Volume
+        <input type="number" bind:value={volume} />
+      </label>
+    </div>
+    <div>
+      <button onclick={sendBuyRequest}>BUY</button>
+      <button onclick={sendSellRequest}>SELL</button>
+    </div>
   </div>
-
-  <h1>Order book</h1>
-  <ul>
-    {#each snapshot.bids as bid}
-      <li>{JSON.stringify(bid)}</li>
-    {/each}
-  </ul>
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <button onclick={sendRequest}>Send order</button>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  div.OrderBook {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+
+  .OrderBook > li {
+    list-style: none;
   }
 </style>
