6bfb0e6f9fe43dcc8ca8ebccad440bd13243bc97
thomas-god <thomas.godlewski@icloud.com>
2025-02-05 15:57:57 +0100

    chore: update js tooling

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,12 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  test: {
+    include: ["**/*.test.ts"],
+    globals: true
+  }
 })
