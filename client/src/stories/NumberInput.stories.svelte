3b436ebe719d0ee2bbddabfd4fc5dc93c04a9e51
thomas-god <thomas.godlewski@icloud.com>
2025-02-06 16:27:11 +0100

    feat: add Storybook to front

---
+++ client/src/stories/NumberInput.stories.svelte
@@ -0,0 +1,24 @@
+<script module>
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import { none, some } from "../Options";
+  import NumberInput from "../NumberInput.svelte";
+
+  let value = $state(0);
+
+  const { Story } = defineMeta({
+    title: "Atoms/NumberInput",
+    component: NumberInput,
+    tags: ["autodocs"],
+    parameters: {
+      layout: "fullscreen",
+    },
+    args: {
+      bigIncr: none(),
+      label: "Label",
+    },
+  });
+</script>
+
+<Story name="Default" args={{ value }} />
+
+<Story name="With buttons" args={{ value, bigIncr: some(10) }} />
