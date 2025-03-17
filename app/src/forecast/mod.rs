c3c546ba1bd87156cceaddcbcc0160031b9c2ad2
thomas-god <thomas.godlewski@icloud.com>
2025-03-17 15:05:34 +0100

    chore(app): remove `ForecastLevel`

--- app/src/forecast/mod.rs
+++ app/src/forecast/mod.rs
@@ -5,29 +5,6 @@ use crate::{
     plants::technologies::timeseries::Timeseries,
 };
 
-#[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Serialize)]
-pub enum ForecastLevel {
-    Low,
-    Medium,
-    High,
-}
-
-/// Classify a value to a [ForecastLevel].
-pub fn map_value_to_forecast_level(value: isize, max: isize) -> ForecastLevel {
-    if max > 0 {
-        return match value {
-            value if value < max / 3 => ForecastLevel::Low,
-            value if value > 2 * max / 3 => ForecastLevel::High,
-            _ => ForecastLevel::Medium,
-        };
-    }
-    match value {
-        value if value > max / 3 => ForecastLevel::Low,
-        value if value < 2 * max / 3 => ForecastLevel::High,
-        _ => ForecastLevel::Medium,
-    }
-}
-
 pub type Forecasts = Vec<Forecast>;
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Serialize)]
 pub struct Forecast {
@@ -121,56 +98,11 @@ pub fn forecast_from_timeseries(
 #[cfg(test)]
 mod tests {
     #![allow(unused_comparisons)]
-    use super::{Clip, ForecastLevel, ForecastValue, forecast_value, map_value_to_forecast_level};
+    use super::{Clip, ForecastValue, forecast_value};
     use crate::{
         constants, forecast::forecast_from_timeseries, game::delivery_period::DeliveryPeriodId,
         plants::technologies::timeseries::Timeseries,
     };
-
-    #[test]
-    fn test_map_forecast_level_default_min() {
-        assert_eq!(map_value_to_forecast_level(-1, 1000), ForecastLevel::Low);
-        assert_eq!(map_value_to_forecast_level(0, 1000), ForecastLevel::Low);
-        assert_eq!(map_value_to_forecast_level(332, 1000), ForecastLevel::Low);
-        assert_eq!(
-            map_value_to_forecast_level(333, 1000),
-            ForecastLevel::Medium
-        );
-        assert_eq!(
-            map_value_to_forecast_level(666, 1000),
-            ForecastLevel::Medium
-        );
-        assert_eq!(map_value_to_forecast_level(667, 1000), ForecastLevel::High);
-        assert_eq!(map_value_to_forecast_level(1000, 1000), ForecastLevel::High);
-        assert_eq!(map_value_to_forecast_level(1001, 1000), ForecastLevel::High);
-    }
-
-    #[test]
-    fn test_map_forecast_level_with_negative_min() {
-        assert_eq!(map_value_to_forecast_level(1, -1000), ForecastLevel::Low);
-        assert_eq!(map_value_to_forecast_level(0, -1000), ForecastLevel::Low);
-        assert_eq!(map_value_to_forecast_level(-332, -1000), ForecastLevel::Low);
-        assert_eq!(
-            map_value_to_forecast_level(-333, -1000),
-            ForecastLevel::Medium
-        );
-        assert_eq!(
-            map_value_to_forecast_level(-666, -1000),
-            ForecastLevel::Medium
-        );
-        assert_eq!(
-            map_value_to_forecast_level(-667, -1000),
-            ForecastLevel::High
-        );
-        assert_eq!(
-            map_value_to_forecast_level(-1000, -1000),
-            ForecastLevel::High
-        );
-        assert_eq!(
-            map_value_to_forecast_level(-1001, -1000),
-            ForecastLevel::High
-        );
-    }
 
     #[test]
     fn test_forecast_value() {
