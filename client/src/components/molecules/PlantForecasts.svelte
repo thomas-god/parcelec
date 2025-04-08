16ef4dc5274f4f145f296b42f22a83629c26d244
thomas-god <thomas.godlewski@icloud.com>
2025-04-08 23:07:20 +0200

    feat(client): transpose forecasts table

--- client/src/components/molecules/PlantForecasts.svelte
+++ client/src/components/molecules/PlantForecasts.svelte
@@ -14,7 +14,7 @@
     ? I
     : never)["type"];
 
-  let merged_forecasts = $derived.by(() => {
+  let forecasts_by_plant = $derived.by(() => {
     let merged: [string, PlantType, Forecast][] = [];
     for (const [id, forecast] of forecasts.entries()) {
       if (!!forecast) {
@@ -27,14 +27,40 @@
     return merged;
   });
 
-  let sum_by_period = $derived.by(() => {
+  let consumers_forecasts = $derived.by(() => {
+    const forecasts = new Map<number, number>();
+    for (const [_, plant, forecast] of forecasts_by_plant) {
+      if (plant === "Consumers") {
+        for (const point of forecast) {
+          const f = forecasts.get(point.period) || 0;
+          forecasts.set(point.period, f + point.value.value);
+        }
+      }
+    }
+    return forecasts;
+  });
+
+  let renewable_forecasts = $derived.by(() => {
+    const forecasts = new Map<number, number>();
+    for (const [_, plant, forecast] of forecasts_by_plant) {
+      if (plant === "RenewablePlant") {
+        for (const point of forecast) {
+          const f = forecasts.get(point.period) || 0;
+          forecasts.set(point.period, f + point.value.value);
+        }
+      }
+    }
+    return forecasts;
+  });
+
+  let total_forecasts = $derived.by(() => {
     const sums = new Map<number, number>();
 
     // Initialize sums for all periods
     periods.forEach((period) => sums.set(period, 0));
 
     // Sum up values for each period across all forecasts
-    for (const [_, __, forecast] of merged_forecasts) {
+    for (const [_, __, forecast] of forecasts_by_plant) {
       for (const point of forecast) {
         const currentSum = sums.get(point.period) || 0;
         sums.set(point.period, currentSum + point.value.value);
@@ -45,8 +71,8 @@
   });
 
   let periods = $derived.by(() => {
-    if (merged_forecasts.length > 0) {
-      const [_, __, forecast] = merged_forecasts[0];
+    if (forecasts_by_plant.length > 0) {
+      const [_, __, forecast] = forecasts_by_plant[0];
       return forecast.map((f) => f.period);
     }
     return [];
@@ -58,39 +84,38 @@
     <thead>
       <tr>
         <th class="text-end">Période</th>
-        {#each periods as period (`${forecasts}-head-${period}`)}
-          <th class="text-center">{period}</th>
-        {/each}
+        <th class="text-center"
+          >{PLANT_ICONS["Consumers"]} {PLANT_NAMES["Consumers"]}</th
+        >
+        <th class="text-center"
+          >{PLANT_ICONS["RenewablePlant"]} {PLANT_NAMES["RenewablePlant"]}</th
+        >
+        <!-- <th class="text-center">Total</th> -->
       </tr>
     </thead>
     <tbody>
-      {#each merged_forecasts as [id, plant, forecast] (id)}
+      {#each total_forecasts as [period, _]}
         <tr>
-          <th>
-            <span>
-              {PLANT_ICONS[plant]}
-            </span>
-            {PLANT_NAMES[plant]}
+          <th class="text-end">
+            {period}
           </th>
-          {#each forecast as point (`${id}-row-${point.period}`)}
-            <td class="text-center"
-              >{point.value.value.toLocaleString("fr-FR", {
-                signDisplay: "exceptZero",
-              })} MW</td
-            >
-          {/each}
-        </tr>
-      {/each}
-      <tr class="font-bold">
-        <th class="text-end">Total</th>
-        {#each periods as period (`${forecasts}-total-${period}`)}
           <td class="text-center">
-            {(sum_by_period.get(period) || 0).toLocaleString("fr-FR", {
+            {consumers_forecasts.get(period)!.toLocaleString("fr-FR", {
               signDisplay: "exceptZero",
             })} MW
           </td>
-        {/each}
-      </tr>
+          <td class="text-center">
+            {renewable_forecasts.get(period)!.toLocaleString("fr-FR", {
+              signDisplay: "exceptZero",
+            })} MW
+          </td>
+          <!-- <th class="text-center">
+            {total_forecasts.get(period)!.toLocaleString("fr-FR", {
+              signDisplay: "exceptZero",
+            })} MW
+          </th> -->
+        </tr>
+      {/each}
     </tbody>
   </table>
 </div>
