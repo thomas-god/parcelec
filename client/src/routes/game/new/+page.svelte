6b36ce351c869cc77ffc5a932b8e332985afbc9a
thomas-god <thomas.godlewski@icloud.com>
2025-04-10 14:06:29 +0200

    feat(client): add collapsable options for nuke and battery

--- client/src/routes/game/new/+page.svelte
+++ client/src/routes/game/new/+page.svelte
@@ -8,6 +8,9 @@
   let period_duration_seconds = $state("120");
   let gas_installed_capacity = $state("500");
   let gas_cost = $state("80");
+  let nuclear_installed_capacity = $state("1000");
+  let nuclear_cost = $state("35");
+  let battery_charge = $state("300");
   let apiError = $state("");
 
   let isGameNameValid = $derived(game_name && game_name.trim() !== "");
@@ -27,11 +30,38 @@
     const cost_valid =
       gas_cost !== "" && !isNaN(Number(gas_cost)) && Number(gas_cost) > 0;
 
-    return max_power_valid && gas_cost;
+    return max_power_valid && cost_valid;
+  });
+
+  let areNuclearPlantOptionsValid = $derived.by(() => {
+    const max_power_valid =
+      nuclear_installed_capacity !== "" &&
+      !isNaN(Number(nuclear_installed_capacity)) &&
+      Number(nuclear_installed_capacity) > 0;
+
+    const cost_valid =
+      gas_cost !== "" &&
+      !isNaN(Number(nuclear_cost)) &&
+      Number(nuclear_cost) > 0;
+
+    return max_power_valid && cost_valid;
+  });
+
+  let areBatteryOptionsValid = $derived.by(() => {
+    const max_charge_valid =
+      battery_charge !== "" &&
+      !isNaN(Number(battery_charge)) &&
+      Number(battery_charge) > 0;
+
+    return max_charge_valid;
   });
 
   let isFormValid = $derived(
-    isGameNameValid && isPeriodDurationValid && areGasPlantOptionsValid,
+    isGameNameValid &&
+      isPeriodDurationValid &&
+      areGasPlantOptionsValid &&
+      areNuclearPlantOptionsValid &&
+      areBatteryOptionsValid,
   );
 
   const createGame = async () => {
@@ -44,8 +74,8 @@
           cost: gas_cost,
         },
         nuclear: {
-          max_power: 1000,
-          cost: 35,
+          max_power: nuclear_installed_capacity,
+          cost: nuclear_cost,
         },
         battery: {
           max_charge: 300,
@@ -87,50 +117,85 @@
           />
         </label>
 
-        <div class="divider divider-start">Options</div>
+        <div class="divider divider-start text-sm font-semibold">Options</div>
 
-        <NumericInput
-          title="Durée des périodes (en secondes)"
-          error_message="La durée doit être d'au moins 30 secondes"
-          min_value="30"
-          bind:value={period_duration_seconds}
-        />
-
-        <!-- GAS PLANT OPTIONS -->
-        <div class="divider divider-start">
-          {PLANT_ICONS["GasPlant"]}
-          {PLANT_NAMES["GasPlant"]}
-        </div>
-        <NumericInput
-          title="Puissance installée (en MW)"
-          error_message="La puissance doit être > 0"
-          min_value="1"
-          bind:value={gas_installed_capacity}
-        />
-        <NumericInput
-          title="Coût de production (en €/MWh)"
-          error_message="Le coût doit être >= 0"
-          min_value="0"
-          bind:value={gas_cost}
-        />
-
-        <!-- <label class="fieldset-label flex-col">
-          <div class="flex justify-between items-center w-full">
-            <div class="self-start text-sm">
-              Durée des périodes (en secondes)
+        <div class="join join-vertical bg-base-100">
+          <div class="collapse collapse-arrow join-item border-base-300 border">
+            <input type="checkbox" />
+            <div class="collapse-title font-semibold">Général</div>
+            <div class="collapse-content text-sm">
+              <NumericInput
+                title="Durée des périodes (en secondes)"
+                error_message="La durée doit être d'au moins 30 secondes"
+                min_value="30"
+                bind:value={period_duration_seconds}
+              />
             </div>
           </div>
-          <input
-            type="number"
-            min="30"
-            class="input validator text-base"
-            bind:value={period_duration_seconds}
-            required
-          />
-          <p class="validator-hint self-start mt-0">
-            La durée doit être d'au moins 30 secondes
-          </p>
-        </label> -->
+
+          <!-- GAS PLANT OPTIONS -->
+          <div class="collapse collapse-arrow join-item border-base-300 border">
+            <input type="checkbox" />
+            <div class="collapse-title font-semibold">
+              {PLANT_ICONS["GasPlant"]}
+              {PLANT_NAMES["GasPlant"]}
+            </div>
+            <div class="collapse-content text-sm">
+              <NumericInput
+                title="Puissance installée (en MW)"
+                error_message="La puissance doit être > 0"
+                min_value="1"
+                bind:value={gas_installed_capacity}
+              />
+              <NumericInput
+                title="Coût de production (en €/MWh)"
+                error_message="Le coût doit être >= 0"
+                min_value="0"
+                bind:value={gas_cost}
+              />
+            </div>
+          </div>
+
+          <!-- NUCLEAR PLANT OPTIONS -->
+          <div class="collapse collapse-arrow join-item border-base-300 border">
+            <input type="checkbox" />
+            <div class="collapse-title font-semibold">
+              {PLANT_ICONS["Nuclear"]}
+              {PLANT_NAMES["Nuclear"]}
+            </div>
+            <div class="collapse-content text-sm">
+              <NumericInput
+                title="Puissance installée (en MW)"
+                error_message="La puissance doit être > 0"
+                min_value="1"
+                bind:value={nuclear_installed_capacity}
+              />
+              <NumericInput
+                title="Coût de production (en €/MWh)"
+                error_message="Le coût doit être >= 0"
+                min_value="0"
+                bind:value={nuclear_cost}
+              />
+            </div>
+          </div>
+
+          <!-- BATTERY OPTIONS -->
+          <div class="collapse collapse-arrow join-item border-base-300 border">
+            <input type="checkbox" />
+            <div class="collapse-title font-semibold">
+              {PLANT_ICONS["Battery"]}
+              {PLANT_NAMES["Battery"]}
+            </div>
+            <div class="collapse-content text-sm">
+              <NumericInput
+                title="Charge maximale (en MWh)"
+                error_message="La charge doit être > 0"
+                min_value="1"
+                bind:value={battery_charge}
+              />
+            </div>
+          </div>
+        </div>
 
         {#if apiError}
           <div class="alert alert-error mt-4 py-2">
